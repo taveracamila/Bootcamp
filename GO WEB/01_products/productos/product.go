993aa3fdda770925fc7cdf5d700aab9ae3ae289f
Camila Tavera <camila.tavera@mercadolibre.com>
2023-01-05 14:25:00 -0300

    falta importar

---
+++ GO WEB/01_products/productos/product.go
@@ -0,0 +1,15 @@
+package product
+
+type Product struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Quantity    int     `json:"quantity"`
+	CodeValue   string  `json:"code_value"`
+	IsPublished bool    `json:"is_published"`
+	Expiration  string  `json:"expiration"`
+	Price       float64 `json:"price"`
+}
+
+
+
+var Products []Product
